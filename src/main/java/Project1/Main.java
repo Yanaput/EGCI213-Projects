c4a80d5eabd87ee4cbe6e9e8478801ae3c835ee4
Yanaput <nokg957@gmail.com>
2024-05-25 20:00:24 +0700

    Add seperate try-catch block for each file scanning

--- src/main/java/Project1/Main.java
+++ src/main/java/Project1/Main.java
@@ -10,51 +10,60 @@ public class Main {
     }
 
     public void task(){
-        try{
-            //----------------------------------------- products------------------------------------------------//
+        Scanner fileScanner;
+        ArrayList<Product> productArrayList = new ArrayList<Product>();
+        ArrayList<SalePerson> salePersonArrayList = new ArrayList<SalePerson>();
+        //----------------------------------------- products------------------------------------------------//
 
-            Scanner productScanner = new Scanner(new File("src/main/java/Project1/products.txt"));
-            productScanner.nextLine();
+        try {
+            fileScanner = new Scanner(new File("src/main/java/Project1/products.txt"));
+            fileScanner.nextLine();
             System.out.println("Read from src/main/java/Project1/products.txt");
-            ArrayList<Product> productArrayList = new ArrayList<Product>();
 
-            while(productScanner.hasNext()){
-                String readLine = productScanner.nextLine();
+            while (fileScanner.hasNext()) {
+                String readLine = fileScanner.nextLine();
                 String[] cols = readLine.split(",");
-                for(int i = 0 ;i < cols.length ;i++){
+                for (int i = 0; i < cols.length; i++) {
                     cols[i] = cols[i].trim();
                 }
+
                 int[] commRate = {Integer.parseInt(cols[3]), Integer.parseInt(cols[4]), Integer.parseInt(cols[5]),
                         Integer.parseInt(cols[6]), Integer.parseInt(cols[7])};
 
-                productArrayList.add(new Product(cols[0], cols[1], Integer.parseInt(cols[2]) , commRate));
-
+                productArrayList.add(new Product(cols[0], cols[1], Integer.parseInt(cols[2]), commRate));
             }
-
-            for(Product i: productArrayList)
-                i.print();
-
-            //----------------------------------------- SalePersons------------------------------------------------//
-
-            Scanner salePersonScanner = new Scanner(new File("src/main/java/Project1/products.txt"));
-            salePersonScanner.nextLine();
-            System.out.println("Read from src/main/java/Project1/products.txt");
-            ArrayList<SalePerson> salePersonArrayList = new ArrayList<SalePerson>();
-
-            while (productScanner.hasNext()){
-                String readLine = productScanner.nextLine();
-                String[] cols = readLine.split(",");
-                for(int i = 0 ;i < cols.length ;i++){
-                    cols[i] = cols[i].trim();
-                }
-
-            }
-
-            salePersonScanner.close();
-            productScanner.close();
+            fileScanner.close();
         }
         catch (Exception e){
             System.err.println(e);
         }
+
+        for (Product i : productArrayList)
+            i.print();
+
+        //----------------------------------------- SalePersons------------------------------------------------//
+        try {
+
+            fileScanner = new Scanner(new File("src/main/java/Project1/salespersons.txt"));
+            fileScanner.nextLine();
+            System.out.println("Read from src/main/java/Project1/salespersons.txt");
+
+
+            while (fileScanner.hasNext()) {
+                String readLine = fileScanner.nextLine();
+                String[] cols = readLine.split(",");
+                for (int i = 0; i < cols.length; i++) {
+                    cols[i] = cols[i].trim();
+                }
+                salePersonArrayList.add(new SalePerson(cols[0], cols[1]));
+            }
+            fileScanner.close();
+        }
+        catch(Exception e){
+            System.err.println(e);
+        }
+
+        for(SalePerson i: salePersonArrayList)
+            i.print();
     }
 }
