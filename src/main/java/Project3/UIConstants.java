c6aef09a1c4d6e03b3cf636439aa50dbe3ddb285
RottenKiwi <61570087+RottenKiwi2004@users.noreply.github.com>
2024-07-06 13:26:21 +0700

    Create UIConstants.java

---
+++ src/main/java/Project3/UIConstants.java
@@ -0,0 +1,17 @@
+package Project3;
+
+public class UIConstants {
+    public final int Black              =  0x282c34;
+    public final int White              =  0xabb2bf;
+    public final int LightRed           =  0xe06c75;
+    public final int DarkRed            =  0xbe5046;
+    public final int Green              =  0x98c379;
+    public final int LightYellow        =  0xe5c07b;
+    public final int DarkYellow         =  0xd19a66;
+    public final int Blue               =  0x61afef;
+    public final int Magenta            =  0xc678dd;
+    public final int Cyan               =  0x56b6c2;
+    public final int GutterGrey         =  0x4b5263;
+    public final int CommentGrey        =  0x5c6370;
+    public final int DarkBlueBackground =  0x20293b;
+}
